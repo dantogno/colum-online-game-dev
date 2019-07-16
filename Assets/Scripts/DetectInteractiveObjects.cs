d9a6f699b72465d7ff56bb81ceba117d8ae5597b
David Antognoli <david.antognoli@gmail.com>
2019-07-16 14:40:20 -0500

    crosshair / reticle and detect objects

---
+++ Assets/Scripts/DetectInteractiveObjects.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectInteractiveObjects : MonoBehaviour
+{
+    [SerializeField]
+    private Transform cameraTransform;
+
+    [SerializeField]
+    private float detectionDistance = 3.0f;
+
+    [SerializeField]
+    private LayerMask layersToDetect;
+
+    private InteractiveObject lookedAtObject;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Interact") && lookedAtObject != null)
+        {
+            lookedAtObject.InteractWith();
+        }
+    }
+    private void FixedUpdate()
+    {
+        Debug.DrawRay(cameraTransform.position, cameraTransform.forward * detectionDistance, Color.red);
+        RaycastHit raycastHit;
+        if (Physics.Raycast(cameraTransform.position, cameraTransform.forward, 
+            out raycastHit, detectionDistance, layersToDetect))
+        {
+            // The code within this if statement block only executes if our raycast hits something.
+            Debug.Log("The raycast hit " + raycastHit.collider.gameObject.name);
+
+             lookedAtObject = 
+                raycastHit.collider.gameObject.GetComponent<InteractiveObject>();
+        }
+    }
+}
---
+++ Assets/Scripts/InteractiveObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveObject : MonoBehaviour
+{
+    public void InteractWith()
+    {
+        // Debug.Log("The player interacted with " + name + ".");
+        // Example of string interpolation.
+        Debug.Log($"The player interacted with {name}.");
+    }
+}
