fc2de791e17384fff9c5c05be5c6521dcdd04201
David Antognoli <david.antognoli@gmail.com>
2019-08-06 13:55:45 -0500

    inventory menu show and hide

---
+++ Assets/Scripts/InventoryMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Gameplay;
+
+public class InventoryMenu : MonoBehaviour
+{
+    private CanvasGroup menuCanvasGroup;
+    private PlayerController playerController;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        playerController = FindObjectOfType<PlayerController>();
+        menuCanvasGroup = GetComponent<CanvasGroup>();
+        HideMenu();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Inventory Menu"))
+        {
+            bool isMenuVisible = menuCanvasGroup.alpha > 0;
+            if (isMenuVisible)
+                HideMenu();
+            else
+                ShowMenu();
+        }
+    }
+
+    private void HideMenu()
+    {
+        menuCanvasGroup.alpha = 0;
+        menuCanvasGroup.blocksRaycasts = false;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        playerController.enabled = true;
+    }
+
+    private void ShowMenu()
+    {
+        menuCanvasGroup.alpha = 1;
+        menuCanvasGroup.blocksRaycasts = true;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        playerController.enabled = false;
+    }
+}
